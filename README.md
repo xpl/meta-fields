633537186a23bc251fdd1e5124932859b561032d
xpl <rocket.mind@gmail.com>
2017-04-06 07:18:43 +0300

    initial

---
+++ README.md
@@ -0,0 +1,126 @@
+# Meta-annotations for data structures in JavaScript
+
+```javascript
+it ('Importing', () => {
+
+    Meta = require ('./meta-fields')
+})
+
+it ('Defining tags', () => {
+
+    $foo = Meta.tag ('foo'),
+    $bar = Meta.tag ('bar')
+})
+
+it ("Defining tags in global namespace", () => {
+
+    Meta.globalTag ('qux')
+})
+
+it ('Tag is a function, accepting random data', () => {
+
+    $foo (42)           // returns 42 tagged with $foo
+    $foo ($bar (42))    // returns 42 tagged with $foo and $bar
+})
+
+it ('It returns a wrapper holding the metadata and the original value', () => {
+
+    const x = $foo (42)
+
+    assert (Meta.is (x), true)
+
+    assert (            x,   { wrapped: 42, __meta__: { foo: true } })
+    assert (      $bar (x),  { wrapped: 42, __meta__: { foo: true, bar: true } })
+    assert ($qux ($bar (x)), { wrapped: 42, __meta__: { foo: true, bar: true, qux: true } })
+})
+
+it ("Order/redundancy is irrelevant", () => {
+
+    assert ($foo ($bar (42)), $bar ($foo ($foo (42))))
+})
+
+it ("Supplying additional metadata along with a tag", () => {
+
+    assert ($foo ('some data', $bar (42)), { wrapped: 42, __meta__: { foo: 'some data', bar: true } })
+})
+
+it ("Explicit way of adding tags", () => {
+
+    assert (Meta.setTags (42, { foo: true }), $foo (42))
+})
+
+it ("Accessing the original value", () => {
+
+    assert (Meta.unwrap ($foo ($bar (42))), 42)
+    assert (Meta.unwrap (42),               42)
+})
+
+it ("Modifying tagged value while keeping the original tags intact (can also add new tags this way)", () => {
+
+    assert (Meta.modify (      42,  x =>       x + 1),              43)
+    assert (Meta.modify ($foo (42), x =>       x + 1),  $foo (      43))
+    assert (Meta.modify ($foo (42), x => $bar (x + 1)), $foo ($bar (43)))
+})
+
+it ("Tagging 'nothing' is legal. And it is not the same as tagging 'undefined'...", () => {
+
+    assert ($foo (),          { __meta__: { foo: true } })
+    assert ($foo (undefined), { __meta__: { foo: true }, wrapped: undefined })
+
+    assert (Meta.hasValue ($foo ())         , false)
+    assert (Meta.hasValue ($foo (undefined)), true)
+})
+
+it ("Checking for a specific tag presence", () => {
+
+    assert ($foo.is (      42),  false)
+    assert ($foo.is ($foo (42)), true)
+    assert ($foo.is ($bar (42)), false)
+
+    assert (Meta.hasTag ($foo (42), 'foo'), true)
+})
+
+it ("Obtaining a value associated with a tag", () => {
+
+    assert ($foo.read (              42),   undefined)
+    assert ($foo.read ($foo (        42)),  true)
+    assert ($foo.read ($foo ('data', 42)), 'data')
+
+    assert (Meta.readTag ($foo ('data', 42), 'foo'), 'data')
+})
+
+it ("Getting all tags", () => {
+
+    assert (Meta.tags ($foo ($bar ($qux (42)))), { 'qux': true, 'bar': true, 'foo': true })
+    assert (Meta.tags (                  42),    {})
+})
+
+it ("Enumerating tags", () => {
+
+    const calls = []
+
+    Meta.eachTag ($foo ($bar ($qux (42))), (tag, value) => calls.push ([tag, value]))
+
+    assert (calls, [['qux', true], ['bar', true], ['foo', true]])
+})
+
+it ("Removing tags", () => {
+
+    assert (Meta.omitTags ($foo ($bar (7)), 'foo'), $bar ( 7))
+    assert (Meta.omitTags (            7,   'foo'),        7)
+})
+
+it ("Using with string.ify (if available)", () => {
+
+    const stringify = require ('string.ify'),
+          pretty    = require ('string.ify').configure ({ pretty: true })
+
+    assert (stringify ( { foo: $foo ($qux ({ bar: 7 }, 1)) }),
+                       '{ foo: $foo ($qux ({ bar: 7 }, 1)) }')
+    
+    assert (pretty ( { foo: $foo ($qux ([ 7, 8 ])) }),
+
+                    '{ foo: $foo ($qux ([ 7,'          + '\n' +
+                    '                     8  ])) }')
+})
+```
---
+++ meta-fields.js
@@ -0,0 +1,152 @@
+"use strict";
+
+/*  ------------------------------------------------------------------------ */
+
+    const O = Object
+
+/*  ------------------------------------------------------------------------ */
+
+    const pickFields = (obj, predicate) => {
+
+        const result = {}
+
+        for (let k of Object.getOwnPropertyNames (obj)) {
+            const v = obj[k]
+            if (predicate (k, v)) { result[k] = v }
+        }
+
+        return result
+    }
+
+/*  ------------------------------------------------------------------------ */
+
+    class Meta {
+
+        constructor (x) {
+
+            this.assign (x)
+        }
+
+        assign (x) {
+
+            if ('wrapped' in x) { this.wrapped = x.wrapped }
+
+            this.__meta__ = x.__meta__ || {}
+
+            return this
+        }
+
+        [Symbol.for ('String.ify')] (stringify) {
+
+            if (stringify.json) {
+
+                return stringify (this.wrapped)
+            }
+
+            let left = ''
+            let numMeta = 0
+
+            for (let tag in this.__meta__) {
+
+                const name = '$' + tag
+                const value = this.__meta__[tag]
+
+                left = (typeof value === 'boolean')
+                            ? (name + ' (' + left)
+                            : (name + ' (' + stringify.configure ({ pretty: false }) (value) + ', ' + left)
+
+                numMeta++
+            }
+
+            const bullet = require ('string.bullet')
+
+            return bullet (left, stringify (this.wrapped)) + ')'.repeat (numMeta)
+        }
+    }
+
+/*  ------------------------------------------------------------------------ */
+
+    module.exports = O.assign (Meta, {
+
+        new: x => new Proxy (new Meta (x), {
+
+            get (target, prop, proxy) {
+
+                if (prop[0] === '$') {
+
+                    throw new Error ('accessing ' + prop + ' — IMPLEMENTATION CHANGED, YOUR CODE IS OBSOLETE')
+                }
+
+                return target[prop]
+            }
+        }),
+
+        coerce: function (x) { return Meta.is (x) ? x : Meta.new ((arguments.length > 0) ? { wrapped: x } : {}) },
+
+        assign: (x, fields) => (Meta.new (Meta.is (x) ? x : { wrapped: x })).assign (fields),
+
+        is: x => (x && (x['__meta__'] !== undefined)) || false,
+
+        hasValue: x => ('wrapped' in Meta.coerce (x)),
+
+        unwrap: x => Meta.coerce (x).wrapped,
+
+        hasTag: (obj, tag) => Meta.is (obj) && (tag in obj.__meta__),
+
+        readTag: (obj, tag) => Meta.is (obj) ? obj.__meta__[tag] : undefined,
+
+        tags: x => Meta.coerce (x).__meta__,
+
+        eachTag: (x, fn) => {
+            const tags = Meta.tags (x)
+            for (let i in tags) { fn (i, tags[i]) }
+        },
+
+        replaceTags: (x, tags) => Meta.assign (x, { __meta__: tags }),
+
+        setTags: (x, tags) => Meta.replaceTags (x, O.assign ({}, Meta.tags (x), tags)),
+
+        setTag: (name, data, ...toWhat) => Meta.setTags (Meta.coerce (...toWhat), { [name]: data }),
+
+        merge: (a, b) => (Meta.is (a) || Meta.is (b))
+                            ? Meta.replaceTags (b, O.assign ({}, Meta.tags (a), Meta.tags (b)))
+                            : b,
+
+        omitTags (x, ...names) {
+
+            if (!Meta.is (x)) { return x }
+
+            const $names = new Set (names)
+
+            return Meta.replaceTags (x, pickFields (x.__meta__, k => !$names.has (k)))
+        },
+
+        modify: (x, fn) => Meta.merge (x, fn (Meta.unwrap (x))),
+
+        tag: (k, impl) => {
+
+            const defaultImpl = (...args) => (args.length < 2)
+                                                ? Meta.setTag (k, true, ...args) // $tag (value)
+                                                : Meta.setTag (k,       ...args) // $tag (data, value)
+
+            return O.assign ((typeof impl === 'function') ? impl.bind (null, k) : defaultImpl, {
+
+               tagName: k,
+                  read: x =>  Meta.readTag (x, k),
+                    is: x =>  Meta.hasTag  (x, k),
+                 isNot: x => !Meta.hasTag  (x, k),
+            })
+        },
+
+        globalTag: (name, ...args) => {
+
+            const isBrowser = (typeof window !== 'undefined') && (window.window === window) && window.navigator,
+                  globalNamespace = isBrowser ? window : global
+
+            return globalNamespace['$' + name] || (globalNamespace['$' + name] = Meta.tag (name, ...args))
+        },
+    })
+
+/*  ------------------------------------------------------------------------ */
+
+
